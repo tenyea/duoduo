02f5d82e35df4bedcb359bce2db8dc277ea8aabf
zzw19880707 <zzw414851474@qq.com>
2014-04-17 17:22:38 +0800

    初始化model

---
+++ duoduo1.0.1/duoduo/CourseModel.m
@@ -0,0 +1,13 @@
+//
+//  CourseModel.m
+//  duoduo
+//
+//  Created by tenyea on 14-4-17.
+//  Copyright (c) 2014年 zzw. All rights reserved.
+//
+
+#import "CourseModel.h"
+
+@implementation CourseModel
+
+@end
