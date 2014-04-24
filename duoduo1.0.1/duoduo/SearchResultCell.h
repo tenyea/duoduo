68f723f6cffef9c022eedc2d285b270920af8e05
zzw19880707 <zzw414851474@qq.com>
2014-04-24 08:41:12 +0800

    新增查询结果cell

---
+++ duoduo1.0.1/duoduo/SearchResultCell.h
@@ -0,0 +1,13 @@
+//
+//  SearchResultCell.h
+//  duoduo
+//
+//  Created by tenyea on 14-4-24.
+//  Copyright (c) 2014年 zzw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchResultCell : UITableViewCell
+
+@end
