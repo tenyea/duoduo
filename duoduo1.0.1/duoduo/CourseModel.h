10200c3bcf65f1891858878179ce381a5c2ad964
zzw19880707 <zzw414851474@qq.com>
2014-04-28 13:45:35 +0800

    修改数据类型

--- duoduo1.0.1/duoduo/CourseModel.h
+++ duoduo1.0.1/duoduo/CourseModel.h
@@ -14,7 +14,7 @@
 @property (nonatomic,retain)NSString *courseName;	//String	课程名称
 @property (nonatomic,retain)NSString *course_images;//	String	课程图片
 @property (nonatomic,retain)NSString *course_price;//	Float	课程售价（打折后）
-@property (nonatomic,retain)NSString *course_price_discount;//	Float	课程折扣
+@property (nonatomic,retain)NSNumber *course_price_discount;//	Float	课程折扣
 @property (nonatomic,retain)NSString *course_sell_price	;//Float	课程原价
 @property (nonatomic,retain)NSString *Description;//	String	课程描述
 @end
