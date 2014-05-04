eef05af23d62030c7e418ab8f6da680e61bcc20d
zzw19880707 <zzw414851474@qq.com>
2014-05-04 09:45:23 +0800

    加入标题

--- duoduo1.0.1/duoduo/CollectionViewController.m
+++ duoduo1.0.1/duoduo/CollectionViewController.m
@@ -19,6 +19,7 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+        self.title = @"收藏课程";
     }
     return self;
 }
--- duoduo1.0.1/duoduo/SignInViewController.m
+++ duoduo1.0.1/duoduo/SignInViewController.m
@@ -19,6 +19,7 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+        self.title = @"签到";
     }
     return self;
 }
