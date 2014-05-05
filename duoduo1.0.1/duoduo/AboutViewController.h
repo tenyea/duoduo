ee0bc4db91b54009153ef3fb2c31e9e8415705b9
zzw19880707 <zzw414851474@qq.com>
2014-05-05 16:31:50 +0800

    添加关于页面

---
+++ duoduo1.0.1/duoduo/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  duoduo
+//
+//  Created by tenyea on 14-5-5.
+//  Copyright (c) 2014年 zzw. All rights reserved.
+//
+
+#import "TenyeaBaseViewController.h"
+
+@interface AboutViewController : TenyeaBaseViewController
+
+@end
---
+++ duoduo1.0.1/duoduo/AboutViewController.m
@@ -0,0 +1,38 @@
+//
+//  AboutViewController.m
+//  duoduo
+//
+//  Created by tenyea on 14-5-5.
+//  Copyright (c) 2014年 zzw. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
