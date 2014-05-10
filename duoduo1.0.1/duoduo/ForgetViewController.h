150fee37a1e76a8ee8d6cb7c6412b81e80aac0c2
zzw19880707 <zzw414851474@qq.com>
2014-05-10 13:07:32 +0800

    忘记密码

---
+++ duoduo1.0.1/duoduo/ForgetViewController.h
@@ -0,0 +1,13 @@
+//
+//  ForgetViewController.h
+//  duoduo
+//
+//  Created by tenyea on 14-5-10.
+//  Copyright (c) 2014年 zzw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ForgetViewController : UITableViewController
+
+@end
